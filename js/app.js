9ed3b7a142a21ebe2385a5fd0ff3b4a0a3b034b7
Jessie Saunders <Saunders@Jessies-MacBook-Air.local>
2017-11-28 20:32:27 -1000

    add toogle function to this page

--- js/app.js
+++ js/app.js
@@ -4,8 +4,38 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navElem = document.getElementsByClassName("navi");
 
+for(var i = 0; i<navElem.length; i++){
+	navElem[i].addEventListener("click", showSecretContents);
+}
 
+function showSecretContents(){
+//console.log(this.innerHTML);
 
+var navItem = this.querySelectorAll(".inner")[0];
+if(navItem.style.display === "none"){
+	navItem.style.display = "block";
+	} else{
+		navItem.style.display = "none";
+	}
+}
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+
+var hElem = document.getElementsByClassName("name");
+
+for(var i = 0; i<hElem.length; i++){
+	hElem[i].addEventListener("click", showSecretMenuItem);
+}
+
+function showSecretMenuItem(){
+//console.log(this.innerHTML);
+
+var menuItem = this.querySelectorAll(".menu")[0];
+if(menuItem.style.display === "none"){
+	menuItem.style.display = "block";
+	} else{
+		menuItem.style.display = "none";
+	}
+}
